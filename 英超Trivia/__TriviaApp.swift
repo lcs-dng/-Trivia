f39fd3e58ae46b56b3c64201829305cec9be64f6
Dexter Ng <DNg@lcs.on.ca>
2025-04-03 12:09:58 -0400

    Initial Commit

---
+++ 英超Trivia/__TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  __TriviaApp.swift
+//  英超Trivia
+//
+//  Created by Dexter Ng on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct __TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
