a7bccff1a9aaf72493bbd96a6daf4c58d35c0b7a
Dexter Ng <DNg@lcs.on.ca>
2025-04-04 15:21:06 -0400

    A feedback of either correct or wrong appears when the user press the "Submit" button.

--- 英超Trivia/Views/GuessingView.swift
+++ 英超Trivia/Views/GuessingView.swift
@@ -8,15 +8,58 @@
 import SwiftUI
 
 struct GuessingView: View {
+    
+    // MARK: Stored properties
+    
+    // What the user is supposed to be guessing
+    @State var currentPlayer = playerToGuess.randomElement()!
+        
+    // What the user enter for answer
+    @State var userGuess = ""
+    
+    // Feedback for the user's answer
+    @State var feedback: String = ""
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            ZStack {
+                Image(currentPlayer.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                
+                Text(feedback)
+                    .font(.system(size: 20))
+                    .background(Color.white)
+                    .cornerRadius(5)
+            }
+            
+            HStack {
+                TextField("Enter the answer", text: $userGuess)
+                
+                Button {
+                    checkGuess()
+                } label: {
+                    Text("Answer")
+                }
+            }
+            
         }
         .padding()
     }
+    
+    func checkGuess() {
+        
+        if userGuess == currentPlayer.playerName {
+            feedback = "✅"
+        } else if userGuess != currentPlayer.playerName {
+            feedback = "❌"
+        }
+        
+    }
+    
 }
 
 #Preview {
